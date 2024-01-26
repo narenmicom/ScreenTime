ed7293517f2c7bf5b409d30db052fd8946963755
narenmicom <narenmicom@gmail.com>
2024-01-26 21:26:49 +0530

    rm end & start

--- start.py
+++
@@ -1,8 +0,0 @@
-import time
-
-time = time.localtime()
-time = str(time.tm_hour) + ":" + str(time.tm_min) + ":" + str(time.tm_sec)
-
-txt_file = open("start.txt","a+")
-txt_file.write(time+'\n')
-print("Start Time" + time)

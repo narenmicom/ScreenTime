ffef7c5ce64a1df84f5b54e728f049ff6d026314
narenmicom <narenmicom@gmail.com>
2024-01-27 00:17:20 +0530

    added Entry & Exit time to DB

--- time.py
+++ time.py
@@ -4,31 +4,36 @@ import sqlite3
 from datetime import date
 
 
+# Getting Current Time
 time = time.localtime()
 time = str(time.tm_hour) + ":" + str(time.tm_min) + ":" + str(time.tm_sec)
+
+# Getting Date
+today = date.today()
+today = today.strftime("%d/%m/%Y")
+
+# Connecting to DB
 connection = sqlite3.connect("screenTime.db")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS EntryExitDetails(date TEXT, entry_time TEXT, exit_time TEXT)")
-today = date.today()
-today = today.strftime("%d/%m/%Y")
 cursor = connection.cursor()
 
 
+# Adding Entry Time
 if(sys.argv[1] == 'start'):
 	cursor.execute("INSERT INTO EntryExitDetails VALUES(?,?,?)",(today,time,"0"))
-	# file = open("start.txt","a+")
-	# file.write(time+'\n')
-	
 
+
+# Adding Exit Time 
 if(sys.argv[1] == 'end'):
-	# file = open("end.txt","a+")
-	# file.write(time+'\n')
-
 	op = cursor.execute("SELECT ROWID from EntryExitDetails WHERE date = ? ORDER BY ROWID DESC LIMIT 1",(today,)).fetchone()
 	rowID = op[0]
 	insertTime = cursor.execute("UPDATE EntryExitDetails SET exit_time = ? WHERE ROWID = ?",(time,rowID,))
 
 
 rows = cursor.execute("SELECT * from EntryExitDetails").fetchall()
+print(rows)
+
 connection.commit()
+
 print("Time : "+time)

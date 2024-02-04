2969b2de876036401730b29d725ccd88358c61ef
narenmicom <narenmicom@gmail.com>
2024-02-04 19:54:57 +0530

    added help command to check command options

--- timer.py
+++ timer.py
@@ -35,6 +35,12 @@ def differenceOfTime(start,end):
 	return seconds 
 
 
+if(len(sys.argv) == 1 or sys.argv[1] == 'help'):
+	print("ScreenTime CLI")
+	print("     used today     -- To find the Screen Time for Today")
+	print('     used on "date" -- To find the Screen Time on that particular day')
+	exit()
+
 
 # Getting Current Time
 time = time.localtime()
@@ -56,8 +62,10 @@ if(sys.argv[1] == 'start'):
 		toCheck = cursor.execute("SELECT exit_time FROM sessions ORDER BY ROWID DESC LIMIT 1").fetchall()
 		toCheck = len(toCheck[0][0])
 		exit if toCheck == 1 else cursor.execute("INSERT INTO sessions VALUES(?,?,?,?)",(today,time,"0",0))
+		connection.commit()
 	except:
 		cursor.execute("INSERT INTO sessions VALUES(?,?,?,?)",(today,time,"0",0))
+		connection.commit()
 
 # Adding Exit Time
 if(sys.argv[1] == 'end'):
@@ -65,6 +73,7 @@ if(sys.argv[1] == 'end'):
 	startTime = cursor.execute("SELECT entry_time from sessions WHERE ROWID = ?",(rowID,)).fetchone()[0]
 	seconds = differenceOfTime(start=startTime,end=time)
 	insertTime = cursor.execute("UPDATE sessions SET exit_time = ? , diff = ? WHERE ROWID = ?",(time,seconds,rowID,))
+	connection.commit()
 
 if(sys.argv[1] == 'used'):
 	if(sys.argv[2] == 'today'):
